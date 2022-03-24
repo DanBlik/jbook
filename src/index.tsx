23349f19170459a414ed6489d6e165ef28ce760d
Бликов Данила Дмитриевич <DDBlikov@sberbank.ru>
2022-03-24 12:57:03 +0300

    add esbuild for css and js packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,57 @@
-import * as esbuild from 'esbuild-wasm'
-import { useEffect, useState, useRef } from 'react'
-import ReactDOM from 'react-dom'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
-  const ref = useRef<any>()
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
-    ref.current  = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
-    })
-  }
-
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
   useEffect(() => {
-    startService()
-  }, [])
+    startService();
+  }, []);
 
-  const onClickHandler = async () => {
-    if (!ref.current) return
-
-    // const result = await ref.current.transform(input , {
-    //   loader: 'jsx',
-    //   target: 'es2015'
-    // })
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
-    })
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-    setCode(result.outputFiles[0].text)
-  }
+    // console.log(result);
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value)
-  }
+    setCode(result.outputFiles[0].text);
+  };
 
   return (
     <div>
-      <textarea value={input} onChange={onChangeHandler}></textarea>
-
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
-        <button onClick={onClickHandler}>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
-
       <pre>{code}</pre>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
