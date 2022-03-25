5be8f4eda33b4d1b91f9fdc3bc16838dc7dc1d0b
Бликов Данила Дмитриевич <DDBlikov@sberbank.ru>
2022-03-25 10:57:50 +0300

    run code from textarea to iframe

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { fetchPlugin } from './plugins/fetch-plugin'
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const ref = useRef<any>()
+  const iframeRef = useRef<any>()
+  const [input, setInput] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
+    })
+  }
   useEffect(() => {
-    startService();
-  }, []);
+    startService()
+  }, [])
 
   const onClick = async () => {
     if (!ref.current) {
-      return;
+      return
     }
 
     const result = await ref.current.build({
@@ -33,12 +33,30 @@ const App = () => {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
       },
-    });
+    })
 
-    // console.log(result);
+    iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+  }
 
-    setCode(result.outputFiles[0].text);
-  };
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', e => {
+            try {
+              eval(e.data)
+            } catch(err) {
+              const root = document.querySelector('#root')
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error </h4>' + err + '</div>'
+              throw err
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `
 
   return (
     <div>
@@ -49,9 +67,14 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title='preview'
+        ref={iframeRef}
+        srcDoc={html}
+        sandbox='allow-scripts'
+      />
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'))
